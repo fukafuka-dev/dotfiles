1801632e699fdc46503f14ad13da78ece46f6eb9
Your Name <you@example.com>
2019-08-11 09:43:34 +0000

    nvim config

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 repodir=~/dev/github.com/albtrs/dotfiles
-mkdir ~/bin
+mkdir -p ~/bin
 ln -sf $repodir/bin/dialy.zsh ~/bin/dialy.zsh
 ln -sf $repodir/bin/show_colors.sh ~/bin/show_colors.sh
 ln -sf $repodir/.zshrc ~/.zshrc
 ln -sf $repodir/.vimrc ~/.vimrc
 ln -sf $repodir/.tigrc ~/.tigrc
 ln -sf $repodir/.tmux.conf ~/.tmux.conf
+
+mkdir -p ~/.config
+ln -s ~/.vim ~/.config/nvim
+ln -s ~/.vimrc ~/.config/nvim/init.vim
