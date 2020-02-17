e6858df44a7104f45a902021d869753e91019b14
albtrs <yidecode@gmail.com>
2020-02-17 19:01:08 +0900

    update memo and tig

--- bin/memo.py
+++ bin/memo.py
@@ -24,8 +24,8 @@ def agrep(word, work_dir):
 def make_dir(work_dir):
     subprocess.call(["mkdir", "-p", work_dir])
 
-def open_memo(filename):
-    subprocess.call(["vim", filename])
+def open_memo(filename, work_dir):
+    subprocess.call(["vim", filename], cwd=work_dir)
 
 def select_file_with_fzf(work_dir):
     proc = subprocess.run(f"ls {work_dir} | fzf --preview \"head -100 {work_dir}/{{}}\"", shell=True, stdout=subprocess.PIPE)
@@ -34,23 +34,28 @@ def select_file_with_fzf(work_dir):
 def main():
     args = parse_args()
 
+    # config
     if args.config == False:
         print(default_memo_dir())
         sys.exit()
 
     work_dir = memo_dir(args.config) if args.config else default_memo_dir()
 
+    # grep
     if args.grep:
         agrep(args.grep, work_dir)
         sys.exit()
 
+    # new
     if args.name:
         make_dir(work_dir)
         open_memo(f"{str(work_dir.joinpath(args.name))}.txt")
+
+    # edit
     elif work_dir.is_dir():
         filename = select_file_with_fzf(work_dir)
         if filename:
-            open_memo(f"{work_dir}/{filename}")
+            open_memo(filename, work_dir)
 
 if __name__ == '__main__':
     main()
